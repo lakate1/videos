e25a05eef299683db11871aaa2f671c2e7345ebe
Leah Queen <leah836@gmail.com>
2019-07-09 16:40:51 -0600

    video list and item

---
+++ src/components/VideoItem.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const VideoItem = ({ video }) => {
+    return <div>
+    {video.snippet.title}
+    <img src={video.snippet.thumbnails.medium.url} />
+    </div>
+};
+
+export default VideoItem;
