1ef5c3226b84d35800e5c941b1384a27958a49b1
Leah Queen <leah836@gmail.com>
2019-07-11 10:53:28 -0600

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,10 @@ import VideoDetail from "./VideoDetail";
 class App extends React.Component {
     state = { videos: [], selectedVideo: null };
 
+    componentDidMount() {
+        this.onTermSubmit("buildings");
+    }
+
     onTermSubmit = async (term) => {
         const response = await youtube.get("/search", {
             params: {
@@ -14,7 +18,10 @@ class App extends React.Component {
             }
         });
 
-        this.setState({ videos: response.data.items });
+        this.setState({ 
+            videos: response.data.items, 
+            selectedVideo: response.data.items[0]
+        });
     };
 
     onVideoSelect = (video) => {
@@ -26,13 +33,13 @@ class App extends React.Component {
                 <SearchBar onFormSubmit={this.onTermSubmit} />
                 <div className="ui grid">
                     <div className="ui row">
-                        <div className="eleven wide column">
+                        <div className="eleven wide column"> 
                             <VideoDetail video={this.state.selectedVideo} />
                         </div>
                         <div className="five wide column">
                             <VideoList 
-                            onVideoSelect={this.onVideoSelect} 
-                            videos={this.state.videos}
+                                onVideoSelect={this.onVideoSelect} 
+                                videos={this.state.videos}
                      />
                      </div>
                 </div>
--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -8,13 +8,15 @@ const VideoDetail = ({ video }) => {
     const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
 
     return ( //jsx style
+    <div>
         <div className="ui embed">
-            <iframe title="video title" src={videoSrc}/>
+            <iframe title="video player" src={videoSrc} />
+    </div>
         <div className="ui segment">
          <h4 className="ui header">{video.snippet.title}</h4>
          <p>{video.snippet.description}</p>
         </div>
-        </div>
+    </div>
     );
 };
 
